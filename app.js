73b85e8c6bad38c676c58e397ef7a80216e5199f
Somnath Prasad <somnathprasad559@gmail.com>
2020-12-11 14:40:42 +0530

    Updated callback urls for oauth of google and facebook to heroku's root

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ passport.deserializeUser(function(id,done){
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/trackYourProgress",
+    callbackURL: "https://sleepy-anchorage-97310.herokuapp.com/auth/google/trackYourProgress",
     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -94,7 +94,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/trackYourProgress"
+    callbackURL: "https://sleepy-anchorage-97310.herokuapp.com/auth/facebook/trackYourProgress"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
